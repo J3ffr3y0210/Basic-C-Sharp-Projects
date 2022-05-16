21bb4f88f653b727bf44ad4af4244f6345f5fa7c
J3ffr3y0210 <jerhine16@gmail.com>
2022-05-16 13:52:01 -0700

    commit

---
+++ Welcome to Acme Accounting Systems/Welcome to Acme Accounting Systems/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Program
+{
+    static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems\" \"Remember we're Accounting on you");
+        Console.ReadLine();
+        }
+}
---
+++ myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class program
+
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        string name = Console.ReadLine();
+        Console.WriteLine(Remember, We're'"Accounting" + on you);
+        Console.Read();
+    }
+
+}
