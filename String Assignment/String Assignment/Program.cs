f9717566fe6b6abe9f1d0cd037fd4407986cc82d
J3ffr3y0210 <jerhine16@gmail.com>
2022-05-22 13:19:33 -0700

    commit

---
+++ String Assignment/String Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jeff";
+            string qoute = "The man said, \"Hello\", Jeff.";
+            string fileName = @"C:\Users\Jeff";
+            Console.WriteLine(fileName);
+            Console.ReadLine();
+
+            name = name.ToUpper();
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Jeff, I am student of Academt of Learning.");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
